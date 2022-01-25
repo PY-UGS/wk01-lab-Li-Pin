d7be9e01775d4dc4fd17714e84efe4855f4e0112
Li-Pin <97491097+Li-Pin@users.noreply.github.com>
2022-01-25 14:10:09 +0800

    Add files via upload

---
+++ Lab1_Qn2_a.java
@@ -0,0 +1,5 @@
+public class Lab1_Qn2_a {
+    public static void main (String[] args){
+        System.out.println("Hello, I am Lipin!");
+    }
+}
---
+++ Lab1_Qn2_c.java
@@ -0,0 +1,9 @@
+public class Lab1_Qn2_c {
+    public static void main (String[] args){
+        for(int x = 102; x > 65; x -= 1){
+            if(x % 2 != 0){
+                System.out.println("Value of x : " + x);
+            }
+        }
+    }
+}
